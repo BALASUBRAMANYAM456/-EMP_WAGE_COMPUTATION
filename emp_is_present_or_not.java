451c90bc2769cc05d2321e9d79c6c7c9e1a5204d
subramanyam <sbalasubramanyam98@gmail.com>
2022-07-14 16:33:15 +0530

    emp_is_present_or_not

---
+++ emp_is_present_or_not.java
@@ -0,0 +1,18 @@
+/* UC-1 check employee is present or absent.*/
+public class EmpWageProblem {
+        public static void main(String[] args) {
+                /* Displaying welcome message in masterbranch. */
+                System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM.\n");
+
+                int isPresent = 1;
+                 /*employee check */
+                double empCheck = Math.floor(Math.random()*10%2);
+
+                if(empCheck == isPresent) {
+                        System.out.println("EMPLOYEE IS PRESENT.");
+                }
+                else {
+                        System.out.println("EMPLOYEE IS ABSENT.");
+                }
+        }
+}
